c0fd355b542ae2bdc9de3b81f44773e5be64fbe4
vagrant <vagrant@precise32.(none)>
2015-02-06 21:42:14 +0000

    putting it together lab

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,10 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	console.log(data);
+	console.log(req.query.name + "testing");
+	console.log(req.query.description);
+	var newFriend = {"name":req.query.name,"description":req.query.description,"imageURL":"https://imgflip.com/readImage?iid=412211"};
+	data["friends"].push(newFriend);
+	res.render('add', data);
  }
